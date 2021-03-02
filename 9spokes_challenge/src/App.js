fc63e1087b5822fc3fab6d149c8f8be12fcf68fe
Shaqo99 <52594895+Shaqo99@users.noreply.github.com>
2021-03-02 19:56:40 +1300

    added net profit margin and working capital equation

--- 9spokes_challenge/src/App.js
+++ 9spokes_challenge/src/App.js
@@ -6,7 +6,13 @@ function App() {
   var revenueTotal = 0.0;
   var expenseTotal = 0.0;
   var salesTotal = 0.0;
+  var grossProfitMargin = 0.0;
   var netProfitMargin = 0.0;
+  var assetTotal = 0.0;
+  var assetSubtraction = 0.0;
+  var liabilityTotal = 0.0;
+  var liabilityTotalSubtraction = 0.0;
+  
   const [data,setData] = useState([]);
   const fetchData=()=>{
     fetch('data.json'
@@ -24,21 +30,51 @@ function App() {
         console.log(jsonData);
         setData(jsonData);
         Array.from(jsonData.data).forEach(data => {
-          if (data.account_category == 'revenue')
+          if (data.account_category === 'revenue')
             revenueTotal += data.total_value;
-          else if (data.account_category == 'expense')
+          else if (data.account_category === 'expense')
             expenseTotal += data.total_value;
-          else if (data.account_category == 'sales' && data.value_type == 'debit')
+          else if (data.account_type === 'sales' && data.value_type === 'debit')
             salesTotal += data.total_value;
+          else if (data.account_category === 'assets' && data.value_type === 'debit'){
+            if (data.account_type === 'current' || data.account_type === 'bank' || data.account_type === 'current_accounts_receivable')
+            assetTotal += data.total_value;
+          }
+          else if (data.account_category === 'assets' && data.value_type === 'credit'){
+            if (data.account_type === 'current' || data.account_type === 'bank' || data.account_type === 'current_accounts_receivable')
+            assetSubtraction += data.total_value;
+          }
+          else if (data.account_category === 'liability' && data.value_type === 'credit'){
+            if (data.account_type === 'current' || data.account_type === 'bank' || data.account_type === 'current_accounts_receivable')
+            liabilityTotal += data.total_value;
+          }
+          else if (data.account_category === 'liability' && data.value_type === 'debit'){
+            if (data.account_type === 'current' || data.account_type === 'bank' || data.account_type === 'current_accounts_receivable')
+            liabilityTotalSubtraction += data.total_value;
+          }
+
+
         });
         console.log(revenueTotal);
         console.log(expenseTotal);
+        grossProfitMargin = (salesTotal / revenueTotal) * 100;
         console.log(salesTotal);
-        netProfitMargin = (expenseTotal - revenueTotal)/ revenueTotal;
-        console.log(netProfitMargin);
-        //console.log(jsonData)
+        console.log(grossProfitMargin);
+        netProfitMargin = ((expenseTotal - revenueTotal)/ revenueTotal) * 100;
         
-    }); 
+        console.log(netProfitMargin);
+        console.log(assetSubtraction);
+        console.log(assetTotal);
+        assetTotal = assetTotal - assetSubtraction;
+        console.log(assetTotal);
+        console.log(liabilityTotalSubtraction);
+        console.log(liabilityTotal);
+        liabilityTotal = liabilityTotal - liabilityTotalSubtraction;
+        console.log(liabilityTotal);
+        
+        //console.log(jsonData)
+    });
+     
   }
   useEffect(()=>{
       fetchData()
@@ -46,9 +82,7 @@ function App() {
 
   return (
     <div className="App">
-     {/* {
-      data && data.length>0 && data.map((item)=><p>{item.about}</p>)
-     }  */}
+      
     </div>
   );
 }
