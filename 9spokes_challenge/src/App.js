ef7063774dc0169f63cc00c274cf11bd466213ce
Shaqo99 <52594895+Shaqo99@users.noreply.github.com>
2021-03-02 17:10:18 +1300

    moved json file to public folder. implemented file parse function in app.js

--- 9spokes_challenge/src/App.js
+++ 9spokes_challenge/src/App.js
@@ -1,23 +1,44 @@
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let revenueTotal = 0;
+  const [data,setData] = useState([]);
+  const fetchData=()=>{
+    fetch('data.json'
+    ,{
+        headers : {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    })
+    .then(function(res){
+        console.log(res)
+        return res.json();
+    })
+    .then(function(jsonData) { 
+        console.log(jsonData);
+        setData(jsonData);
+        Array.from(jsonData.data).forEach(data => {
+          if (data.account_category == 'expense')
+            revenueTotal += data.total_value;
+             
+        });
+        console.log(revenueTotal);
+        //console.log(jsonData)
+        
+    }); 
+  }
+  useEffect(()=>{
+      fetchData()
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* {
+      data && data.length>0 && data.map((item)=><p>{item.about}</p>)
+     }  */}
     </div>
   );
 }
